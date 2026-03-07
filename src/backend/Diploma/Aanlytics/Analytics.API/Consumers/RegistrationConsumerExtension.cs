c7b2b7b57d8858f225e7495e558de360f2031c51
Vadim Gan <vadim.gan2018@yandex.ru>
2026-03-07 14:05:35 +0300

    # add extension for registration kafka consumer

---
+++ src/backend/Diploma/Aanlytics/Analytics.API/Consumers/RegistrationConsumerExtension.cs
@@ -0,0 +1,17 @@
+﻿using Common.Infrastructure.Kafka;
+using Organization.ApplicationContract.Messages;
+
+namespace Analytics.API.Consumers;
+
+public static class RegistrationConsumerExtension
+{
+    public static IServiceCollection AddKafkaConsumers(this IServiceCollection services, IConfigurationSection configuration)
+    {
+        services.Configure<KafkaConfig>(configuration);
+
+        services.AddHostedService<KafkaConsumer<CreateOrderMessage>>();
+        services.AddScoped<IMessageHandler<CreateOrderMessage>, OrderAnalyticsConsumer>();
+        
+        return services;
+    }
+}
