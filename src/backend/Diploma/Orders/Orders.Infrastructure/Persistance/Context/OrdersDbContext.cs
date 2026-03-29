527a80ec470a8eee87225f290081b7945a741896
Vadim Gan <vadim.gan2018@yandex.ru>
2026-03-29 17:54:28 +0300

    # add orders db context

---
+++ src/backend/Diploma/Orders/Orders.Infrastructure/Persistance/Context/OrdersDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Orders.Domain.Models;
+
+namespace Orders.Infrastructure.Persistance.Context;
+
+public class OrdersDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    
+    public OrdersDbContext(DbContextOptions options) 
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.ConfigureWarnings(warnings => warnings.Log(RelationalEventId.PendingModelChangesWarning));
+    }
+}
